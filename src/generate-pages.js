874ef5b154d58d9f741d8220ba5fbfb84104aaa4
TristanBarrow <tristanmarkbarrow@gmail.com>
2023-11-11 12:22:25 -0700

    improved html gen

--- src/generate-pages.js
+++ src/generate-pages.js
@@ -1,43 +1,52 @@
-const fs = require('fs')
+const fs = require("fs");
 
-const CONFIG_PATH = './config.json'
-const TEMPLATE_PATH = './src/template.html'
-const OUTPUT_DIR_PATH = './docs'
-const TEMPLATE_REPLACEABLE_STRING = '// GLOBALS'
+const CONFIG_PATH = "./config.json";
+const TEMPLATE_PATH = "./src/template.html";
+const OUTPUT_DIR_PATH = "./docs";
+const TEMPLATE_REPLACEABLE_STRING = "// GLOBALS";
 
 // read the template file
-const template = fs.readFileSync(TEMPLATE_PATH).toString()
+const template = fs.readFileSync(TEMPLATE_PATH).toString();
 
 // read the config file
-const config = JSON.parse(fs.readFileSync(CONFIG_PATH).toString())
+const config = JSON.parse(fs.readFileSync(CONFIG_PATH).toString());
 
 // do some checks on the config and throw errors if failed
-config.pages.map(page => {
-    if (page.id === 'index') {
-        throw new Error('!!AMY!! You can\'t name a page "index"')
+config.pages.map((page) => {
+    if (page.id === "index") {
+        throw new Error('!!AMY!! You can\'t name a page "index"');
     }
-    if (page.id.split(' ').length > 1) {
-        throw new Error("!!AMY!! You can't have spaces in a page ID")
+    if (page.id.split(" ").length > 1) {
+        throw new Error("!!AMY!! You can't have spaces in a page ID");
     }
-})
+});
 
 // get all page ids
-const pageIds = config.pages.map(page => page.id)
-const set = new Set(pageIds)
+const pageIds = config.pages.map((page) => page.id);
+const set = new Set(pageIds);
 if (pageIds.length !== set.size) {
-    throw new Error("!!AMY!! You can't have multiple pages with the same ID")
+    throw new Error("!!AMY!! You can't have multiple pages with the same ID");
 }
+
 // delete all the html files from the output folder
 // note: forEach might try and do things async and I'd rather keep things sync for this so I'm using map
-fs.readdirSync(OUTPUT_DIR_PATH).map(item => {
-    if (item.endsWith('.html')) fs.unlinkSync(`${OUTPUT_DIR_PATH}/${item}`)
-})
+fs.readdirSync(OUTPUT_DIR_PATH).map((item) => {
+    if (item.endsWith(".html")) fs.unlinkSync(`${OUTPUT_DIR_PATH}/${item}`);
+});
+
+// after Error checks and cleaning up the html files
+// generate a new home or index.html
+const homeTemplate = template.replace(
+    TEMPLATE_REPLACEABLE_STRING,
+    generateTemplateToReplace("home")
+);
+fs.writeFileSync(`${OUTPUT_DIR_PATH}/index.html`, homeTemplate);
 
 // template to replace
 function generateTemplateToReplace(pageId) {
     // add pageId and the config to the page as a variable and an object
     return `const PAGE_ID = '${pageId}'; 
-const config = ${JSON.stringify(config)};`
+            const config = ${JSON.stringify(config)};`;
 }
 
 function injectConfig(pageId) {
@@ -45,12 +54,12 @@ function injectConfig(pageId) {
     const injectedTemplate = template.replace(
         TEMPLATE_REPLACEABLE_STRING,
         generateTemplateToReplace(pageId)
-    )
+    );
     // write that out to an html file
-    fs.writeFileSync(`${OUTPUT_DIR_PATH}/${pageId}.html`, injectedTemplate)
+    fs.writeFileSync(`${OUTPUT_DIR_PATH}/${pageId}.html`, injectedTemplate);
 }
 
 // loop over the pages in the config file and
-config.pages.map(page => {
-    injectConfig(page.id)
-})
+config.pages.map((page) => {
+    injectConfig(page.id);
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,38 @@
-const { readFileSync, writeFile, writeFileSync } = require('fs')
-const path = require('path')
+const path = require("path");
 
-const template = readFileSync('template.html').toString()
-const configFile = readFileSync('./config.json').toString()
-const config = JSON.parse(configFile)
-config.pages.map(page => {
-    const injectedTemplate = template.replace(
-        '// GLOBALS',
-        `const PAGE_ID = '${page.id}'; 
-const config = ${JSON.stringify(config)};`
-    )
-    writeFileSync(`./docs/${page.id}.html`, injectedTemplate)
-})
+require("./src/generate-pages");
 
 const react = {
-    mode: 'production',
-    target: 'web',
-    entry: './src/index.tsx',
+    mode: "production",
+    target: "web",
+    entry: "./src/index.tsx",
     output: {
-        path: path.resolve(__dirname, 'docs'),
-        filename: 'index.js',
+        path: path.resolve(__dirname, "docs"),
+        filename: "index.js",
     },
     module: {
         rules: [
             {
                 test: /\.tsx?$/,
-                use: 'ts-loader',
+                use: "ts-loader",
                 exclude: /node_modules/,
             },
             {
                 test: /\.s[ac]ss$/i,
                 use: [
                     // Creates `style` nodes from JS strings
-                    'style-loader',
+                    "style-loader",
                     // Translates CSS into CommonJS
-                    'css-loader',
+                    "css-loader",
                     // Compiles Sass to CSS
-                    'sass-loader',
+                    "sass-loader",
                 ],
             },
         ],
     },
     resolve: {
-        extensions: ['.tsx', '.ts', '.js'],
+        extensions: [".tsx", ".ts", ".js"],
     },
-}
+};
 
-module.exports = [react]
+module.exports = [react];
